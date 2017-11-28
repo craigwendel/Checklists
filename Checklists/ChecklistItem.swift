dce3fa3df23f22cc01e8b013d996f38724349d8f
Craig Wendel <wendelcraig@gmail.com>
2017-11-28 09:48:05 -0600

    Edit logic for rows to include checked data model and properly display checkmarks in cells. Add checklist object file.

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Craig Wendel on 11/28/17.
+//  Copyright © 2017 Craig Wendel. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
