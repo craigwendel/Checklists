32c7cc76fc4793ff5a8f23fcaf85f0130e71ebe5
Craig Wendel <wendelcraig@gmail.com>
2017-11-29 00:21:53 -0600

    Add new controller for adding items. Edit storyboard and the add item screen.

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Craig Wendel on 11/28/17.
+//  Copyright © 2017 Craig Wendel. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+
+}
