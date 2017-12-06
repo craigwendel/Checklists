8b77dbcfaa303ee9e2b7324b6a03c0e5ab6fdaeb
Craig Wendel <wendelcraig@gmail.com>
2017-12-06 16:18:07 -0600

    Move save/load ability to AllListsViewController. Save data before app terminates or enters background in app delegate.

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -10,23 +10,49 @@ import UIKit
 
 class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
     var lists = [Checklist]()
+    
+    // MARK:- Loading/Saving Code
+    
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        return paths[0]
+    }
+
+    func dataFilePath() -> URL {
+        return documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+
+    func saveChecklists() {
+        let encoder = PropertyListEncoder()
+
+        do {
+            let data = try encoder.encode(lists)
+            try data.write(to: dataFilePath(), options: Data.WritingOptions.atomic)
+        } catch {
+            print("Error encoding item array!")
+        }
+    }
+
+    func loadChecklists() {
+        let path = dataFilePath()
+
+        if let data = try? Data(contentsOf: path) {
+            let decoder = PropertyListDecoder()
+            do {
+                lists = try decoder.decode([Checklist].self, from: data)
+            } catch {
+                print("Error decoding item array!")
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Enable large titles
         navigationController?.navigationBar.prefersLargeTitles = true
-        // Add placeholder data
-        var list = Checklist(name: "Birthdays")
-        lists.append(list)
-
-        list = Checklist(name: "Groceries")
-        lists.append(list)
-
-        list = Checklist(name: "Cool Apps")
-        lists.append(list)
-
-        list = Checklist(name: "To Do")
-        lists.append(list)
+        // Load data
+        loadChecklists()
     }
 
     override func didReceiveMemoryWarning() {
